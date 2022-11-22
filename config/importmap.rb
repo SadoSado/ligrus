0272dd35ccf563b8821c7c9e64674b4028ec1e7c
sadosado <sadosadoushko@gmail.com>
2022-11-22 11:16:56 +0200

    Add Tailwind and set Sing In page

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,10 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.1.1/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-dropdown", to: "https://ga.jspm.io/npm:stimulus-dropdown@2.0.0/dist/stimulus-dropdown.es.js"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.10.0/dist/hotkeys.esm.js"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.50.0/dist/index.js"
+pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.1.0/dist/stimulus-notification.es.js"
